13cff254b9cbae36b3b22a8d3f1f6772f9e22b2f
timmy-ctrl <timmygithub@gmail.com>
2026-06-21 20:12:53 +0800

    Update sat_transaction_details.sql

--- dbt/models/vault/satellites/sat_transaction_details.sql
+++ dbt/models/vault/satellites/sat_transaction_details.sql
@@ -6,3 +6,45 @@
     order_by=['link_hash_key', 'load_date']
   )
 }}
+
+WITH source AS (
+SELECT
+    transaction_hash_key AS link_hash_key,
+    amount,
+    currency,
+    status,
+    category, is_suspicious,
+    load_date,
+    record_source,
+
+    MD5(contact(
+     coalesce(toString(amount), ''),
+     coalesce(toString(currency), ''),
+     coalesce(toString(status), ''),
+     coalesce(toString(category), ''),
+     coalesce(toString(is_suspicious), ''),
+    )) AS hashdiff
+
+FROM {{ ref('raw_transactions') }}
+{{ incremental_filter('load_date') }}
+),
+
+
+deduplicated AS (
+    SELECT *
+    FROM source
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY link_hash_key, hashdiff
+    ORDER BY load_date ASC) = 1
+)
+
+SELECT
+    link_hash_key,
+    amount,
+    currency,
+    status,
+    category,
+    is_suspicious,
+    hashdiff,
+    load_date,
+    record_source
+FROM deduplicated
