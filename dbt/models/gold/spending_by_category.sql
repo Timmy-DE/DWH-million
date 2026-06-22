0ec9c094209fcce0a4227ad50f23e29e3913fa51
Timmy-DE <timkhorishikh98@gmail.com>
2026-06-23 04:58:38 +0800

    Update spending_by_category.sql

--- dbt/models/gold/spending_by_category.sql
+++ dbt/models/gold/spending_by_category.sql
@@ -1,13 +1,12 @@
 {{
-   config(
-      materialized='table',
-      engine='MergeTree()',
-      order_by='category'
-   )
+  config(
+    materialized='table',
+    engine='MergeTree()',
+    order_by='total_amount'
+  )
 }}
 
 SELECT
-    h.merchant_hash_key,
     s.category,
     count(*)                    AS transaction_count,
     sum(s.amount)               AS total_amount,
@@ -18,10 +17,7 @@ SELECT
 FROM {{ ref('link_transaction') }} l
 JOIN {{ ref('sat_transaction_details') }} s
     ON l.link_hash_key = s.link_hash_key
-JOIN {{ ref('hub_merchant') }} h
-    ON l.merchant_hash_key = h.merchant_hash_key
 GROUP BY
-    h.merchant_hash_key,
     s.category
 ORDER BY
     total_amount DESC
